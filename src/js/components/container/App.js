9ae54f6c532d38256816c1e23771c48c9d1b625a
IsmarySalgesM <ismarysalges@gmail.com>
2018-11-02 20:39:22 -0300

    Search Navbar ok

--- src/js/components/container/App.js
+++ src/js/components/container/App.js
@@ -20,25 +20,37 @@ class ImagenApp extends Component {
   componentDidMount() {
     let dataImagen = fetch(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=03ff720509cad0d3a5d787e545a66e5e&tags=
     landscapes&page=${this.state.page}&per_page=30&format=json&nojsoncallback=1`)
-    .then(response => response.json()) // Obtenemos la información en formato Json
-    .then(responseImagen => {
-      this.setState({ // Realizamos el reset de estados
-        imagen: responseImagen.photos.photo, // Obtenemos la información solo de las fotos 
-        page: 1, // Se obtiene la primera pagina
+      .then(response => response.json()) // Obtenemos la información en formato Json
+      .then(responseImagen => {
+        this.setState({ // Realizamos el reset de estados
+          imagen: responseImagen.photos.photo, // Obtenemos la información solo de las fotos 
+          page: 1, // Se obtiene la primera pagina
+        })
       })
-    })
-}
+  }
 
+  getSearch = (e) => {
+    e.preventDefault();
+    let searchImagen = e.target.picture.value;
+    let dataSearch = fetch(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=03ff720509cad0d3a5d787e545a66e5e&tags=${searchImagen}&page=${this.state.page}&per_page=100&format=json&nojsoncallback=1`)
+      .then(response => response.json()) // Obtenemos la información en formato Json
+      .then(responseSearch => {
+        this.setState({ // Realizamos el reset de estados
+          imagen: responseSearch.photos.photo, // Obtenemos la información solo de las fotos 
+          page: 1, // Se obtiene la primera pagina
+        })
+      })
 
+  }
 
 
   render() {
-     console.log(this.state.imagen) 
+    console.log(this.state.imagen)
     return ( // Pasamos la información del arreglo con la información de las fotos
-      <div> 
-       {/* <Search/>  */}
-      <ViewImagen imagen ={this.state.imagen} />  
-    </div>
+      <div>
+        <Search getSearch={this.getSearch} />
+        <ViewImagen imagen={this.state.imagen} />
+      </div>
     );
   }
 }
--- src/js/components/presentational/Search.js
+++ src/js/components/presentational/Search.js
@@ -1,51 +1,18 @@
-    // import React, { Component } from "react";
-    // import { Input } from 'react-input-component';
-    
-    
-    // class Search extends Component {
-    //   constructor(props) {
-    //     super(props);
-    //     this.state = {
-    //       items: [],
-    //       page: 0,
-    //       text: '',
-    //     };
-    
-    //     this.handleChange = this.handleChange.bind(this);
-    //   }
-    
-      
-    
-    //   handleChange = (e) => {
-    //     let onePages = this.state.page + 1;
-    //     this.setState({ text: e.target.value }, () => {
-    //       let dataSearch = fetch(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=03ff720509cad0d3a5d787e545a66e5e&tags=
-    //       ${this.state.text}&page=${onePages}&per_page=30&format=json&nojsoncallback=1`)
-    //       .then(response => response.json()) // Obtenemos la información en formato Json
-    //       .then(responseSearch => {
-    //         this.setState({ // Realizamos el reset de estados
-    //           items: responseSearch // Obtenemos la información solo de las fotos 
-    //         })
-    //       })
-    //     });
-    //   }
-    
-    
-    //   render() {
-    //     console.log(this.state.items)
-    //     return (
-    //       <div>
+import React from 'react';
+
+
+const Search = (props) => {
+  console.log(props)
+    return (
+        <form onSubmit={props.getSearch}>
+          
+                <input text="text" name="picture" />
             
-    //         <input
-    //           onChange={this.handleChange}
-    //           value={this.state.text}
-    //         />
-    //       </div>
-    //     );
-    //   }
-    
-    
-    // }
-    
-    // export default Search;
-    
+            <button className="btnSearch">Buscar</button>
+        </form>
+
+    );
+}
+
+
+export default Search;
